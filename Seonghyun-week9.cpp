ea47044649b16c99cd2d7740b5c5a57aafee25f0
Seonghyun Choi <43017312+kg41csh@users.noreply.github.com>
2018-11-07 00:08:20 -0500

    final

--- Seonghyun-week9.cpp
+++ Seonghyun-week9.cpp
@@ -23,12 +23,10 @@ for ( n = 0; n < NumStudent; n++ ) {    // looping from 0 to 9(10 students)
     cout << "Enter exam result: " ;// Enter students' examresults
     cin  >> examresults[n]; // getting the exam result and adding that into the examresults' arrays
     
-
         if ( examresults[n] >= 50 ) {   
 
             passes[x] = examresults[n]; // if the student passed, add the result to passes
-            x += 1;
-               
+            x += 1;               
         }
 
         else {   
@@ -42,41 +40,39 @@ cout << "   " << endl; // making line space between student and passes
 
 for ( n=0; n< NumStudent; n++) {
     
-    cout << "student " << n+1 << " -----" << examresults[n] << endl; // print arrays' data of students
+    cout << "student " << n+1 << " ----- " << examresults[n] << endl; // print arrays' data of students
 } 
-            
 
-// making line space between student and passes
-cout << "   " << endl;
+cout << "   " << endl; // making line space between student and passes
 
 for ( n =0; n < NumStudent; n++ ) {
     
     cout << "The passes are   " << passes[n];
 
     if (passes[n] >= 80) { 
-        cout << "----" << "grade A" << endl; // print grade A if passes is over 80
+        cout << " ---- " << "grade A" << endl; // print grade A if passes is over 80
     }
-
         else if (passes[n] >= 70) {
-            cout << "----" << "grade B" << endl; // print grade B if passes is over 70
+            cout << " ---- " << "grade B" << endl; // print grade B if passes is over 70
         }
 
         else if (passes[n] >= 60) {
-            cout << "----" << "grade C" << endl; // print grade C if passes is over 60
+            cout << " ---- " << "grade C" << endl; // print grade C if passes is over 60
         }
         else if (passes[n] >= 50) {
-            cout << "----" << "grade D" << endl; // print grade D if passes is over 50
+            cout << " ---- " << "grade D" << endl; // print grade D if passes is over 50
         }
     else {
-        cout << " ----" << "N/A" << endl;
+        cout << "  ---- " << "N/A" << endl;
     }
 }
 
-if (x >= 8) {    //if eight or more students passed print "raise tuition"
+if (x >= 8) {    //if eight or more students passed, print "raise tuition"
 
     cout << "   " << endl; // making line space between passes and failures group
-    cout << "raise tuition because " << x << " students passed." << endl;
-
+    cout << "###########################################" << endl;
+    cout << " Raise tuition -------- "<< x << " students passed " << endl;
+    cout << "###########################################" << endl;
 }
 
 cout << "   " << endl; // making line space between passes and failures group
@@ -85,14 +81,23 @@ for  ( n =0; n < NumStudent; n++ ) {
     cout << "The failures are " << failures[n];
 
         if ((failures[n] > 1) && (failures[n] <50)) {
-            cout << "----" << "grade F" << endl; // print grade F if the failures is under 50
+            cout << " ---- " << "grade F" << endl; // print grade F if the failures is under 50
         } 
 
         else {
-            cout << " ----" << "N/A" << endl;
+            cout << "  ---- " << "N/A" << endl;
         }
 }
 
+if (y >= 5) {    //if five or more students failed, print "Reduce tuition"
+
+    cout << "   " << endl; // making line space between passes and failures group
+    cout << "##########################################" << endl;
+    cout << " Reduce tuition ------- "<< y << " students failed " << endl;
+    cout << "##########################################" << endl;
+}
+
+cout << "   " << endl; // making line space between passes and failures group
 return 0;
 }
 
